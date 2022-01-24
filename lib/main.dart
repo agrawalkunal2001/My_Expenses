e7e3ab25eb887037697cf70aff46c70df19c9937
Plane <plane2001@outlook.com>
2022-01-24 22:49:12 +0530

    Taking user input

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_expenses/transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,6 +24,9 @@ class HomePage extends StatelessWidget {
     Transaction(id: "t2", title: "Clothes", amount: 1000, date: DateTime.now()),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,24 +34,75 @@ class HomePage extends StatelessWidget {
         title: const Text("My Expenses"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Card(
             color: Colors.blue,
             child: Text("Chart!"),
           ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: "Title"),
+                    controller: titleController,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Amount"),
+                    keyboardType: TextInputType.number,
+                    controller: amountController,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Add Transaction!",
+                        style: TextStyle(fontSize: 15),
+                      )),
+                ],
+              ),
+            ),
+          ),
           Column(
             children: transactions.map((tx) {
               return Card(
+                elevation: 5,
                 child: Row(
                   children: <Widget>[
                     Container(
-                      child: Text(tx.amount.toString()),
+                      width: 120,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        "\u{20B9}${tx.amount}", // \u{20B9} is used to get rupee symbol and ${tx.amount} is equivalent to tx.amount.toString()
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tx.title.toString()),
-                        Text(tx.date.toString()),
+                        Text(
+                          tx.title.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        Text(
+                          DateFormat.yMMMMd().format(tx.date as DateTime),
+                          style: TextStyle(color: Colors.grey),
+                        ),
                       ],
                     ),
                   ],
